3e16946d3e524250448b3571d80ecdd5028a720f
SAndreeva <sandreeva@infragistics.com>
2018-06-06 14:43:53 +0300

    feat(grid): customize ghost image, add events, add demo #935

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -177,7 +177,7 @@ const appRoutes = [
         component: GridSampleComponent
     },
     {
-        path: "gridColumnMoving",
+        path: 'gridColumnMoving',
         component: GridColumnMovingSampleComponent
     },
     {
